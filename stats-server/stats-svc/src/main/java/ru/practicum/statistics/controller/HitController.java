c3addd369022870da769f4b7cf5e0b6892f5a462
Kirill Mikhailov <kirill.mikhailov.d@yandex.ru>
2023-12-19 10:37:40 +0300

    fix

--- stats-server/stats-svc/src/main/java/ru/practicum/statistics/controller/HitController.java
+++ stats-server/stats-svc/src/main/java/ru/practicum/statistics/controller/HitController.java
@@ -3,6 +3,7 @@ package ru.practicum.statistics.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.statistics.service.HitServiceImpl;
 import ru.practicum.statistics.util.Util;
@@ -21,6 +22,7 @@ public class HitController {
     private final HitServiceImpl hitService;
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public HitDto saveHit(@Valid @RequestBody HitDto hitDto) {
         log.info("HitController => saveHit: hitDto={}", hitDto);
         return hitService.saveHit(hitDto);
